340869836cceb07397e2f553ac14af0894d3bc6e
vanegomez <vane.gomez@me.com>
2015-05-12 15:35:09 -0600

    I4: added colors to output

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,16 +5,17 @@ NUMBER = rand(100)
 
 def check_guess(guess)
   guess = guess.to_i
-  if guess > NUMBER
+  if guess - NUMBER > 5
+    "Way too high!"
+  elsif NUMBER - guess > 5
+    "Way too low!"
+  elsif guess > NUMBER
     "Too high!"
   elsif guess < NUMBER
     "Too low!"
   elsif guess == NUMBER
     "You got it right! The secret number is #{NUMBER}"
-  elsif guess - NUMBER > 5
-    "Way too high!"
-  elsif NUMBER - guess > 5
-    "Way too low!"
+
   end
 end
 
